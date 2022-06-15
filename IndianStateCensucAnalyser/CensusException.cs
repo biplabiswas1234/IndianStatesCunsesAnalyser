f82a4f3a57d98f2c0249052ec8be89c4b923827c
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-15 12:02:15 +0530

    [Biplab] Add: Analyse to load Indian state census data

---
+++ IndianStateCensucAnalyser/CensusException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensucAnalyser
+{
+    public class CensusException : Exception
+    {
+           public ExceptionType type;
+            public enum ExceptionType
+            {
+                FILE_NOT_EXIST,
+                IMPROPER_EXTENSION,
+                DELIMITER_NOT_FOUND,
+                INCORRECT_HEADER
+            }
+
+        public CensusException(ExceptionType type, string name) : base(name)
+        {
+                this.type = type;
+        }
+        
+    }
+}
---
+++ IndianStateCensucAnalyser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndianStateCensucAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in Indian Census Analyser");
+            
+        }
+    }
+}
